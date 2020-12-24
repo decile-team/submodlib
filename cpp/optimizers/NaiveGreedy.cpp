3e140d155a8ebe86946bde1b320be74d819eb6a5
Vinayak Sharma <bplate3@gmail.com>
2020-12-24 13:09:32 +0530

    Adding prototype code for NaiveGreedy optimizer

--- cpp/optimizers/NaiveGreedy.cpp
+++ cpp/optimizers/NaiveGreedy.cpp
@@ -1,26 +1,47 @@
-/*
-naiveGreedyMax() {
-    Input: SetFunction (f)
-    Input: budget
-    Output: greedyVector
-    
-    greedyVector = empty
-    greedySet = empty
-    remainingBudget = budget
-    while remainingBudget > 0 {
-        best
-        for each element i in f.getEffectiveGroundSet() {
-            if i in greedySet {
-                continue
-            }
-            if f.marginalGainSequential(greedySet, i) > best {
-                best = i;
-            } 
-        } 
-        f.sequentialUpdate(greedySet, best) 
-        greedySet.insert(best)
-        greedyVector.push_back(best)
-        remainingBudget -= 1
-    }
+//Note that this code is in prototype state. Its a direct implementation of the pseudo code and there seems to be some logical errors.
+
+#include<iostream>
+#include<set>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<cmath>
+#include"NaiveGreedy.h"
+
+
+std::vector<float> naiveGreedyMax(FacilityLocation f_obj, float budget)//Possibly we need a template type instead of a fixed type for f_obj 
+{
+	std::vector<float>greedyVector;
+	std::set<ll>greedySet;
+	float rem_budget = budget;
+	
+	while (rem_budget > 0)
+	{
+		//In pseudo code, a single variable "best" was used for storing both marginal gain and datapoint index which seemed incorrect.
+		//Therefore, I have instead used 2 variables viz. best_i and best_val
+		ll best_i;
+		float best_val;
+		for (auto it = f_obj.getEffectiveGroundSet().begin(); it != f_obj.getEffectiveGroundSet().end(); ++it)
+		{
+			ll i = *it;
+			if (greedySet.find(i) != greedySet.end())
+			{
+				continue;
+			}
+			if (f_obj.marginalGainSequential(greedySet, i) > best_val)//Bug: uninitilized variable best_val is getting consumed
+			{
+				best_i = i;
+				best_val = f_obj.marginalGainSequential(greedySet, i);
+			}
+		}
+
+		//Bugs: Uninitilized variable best_i is getting used below.
+		f_obj.sequentialUpdate(greedySet, best_i); 
+		greedySet.insert(best_i); 
+		greedyVector.push_back(best_i);//Verify weather greedyVector contains datapoint indicies or marginal gains
+		--rem_budget;
+	}
+
+	return greedyVector;
 }
-*/
+
---
+++ cpp/optimizers/NaiveGreedy.h
@@ -0,0 +1,7 @@
+//Note that this code is in prototype state. Its a direct implementation of the pseudo code and there seems to be some logical errors.
+
+#include"../submod/FacilityLocation.h"
+
+typedef long long int ll;
+
+std::vector<float> naiveGreedyMax(FacilityLocation f_obj, float budget)
