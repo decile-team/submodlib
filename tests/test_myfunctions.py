5166071443e04f2778e2561b00e8baa94ca80d20
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-16 19:59:01 +0800

    Delete tests directory

--- tests/test_myfunctions.py
+++
@@ -1,9 +0,0 @@
-from submodlib import myfunctions
-def test_squareInt():
-    assert myfunctions.square(5) == 25
-
-def test_squareFloat():
-    assert myfunctions.square(5.0) == 25
-
-def test_squareNegative():
-    assert myfunctions.square(-5.0) == 25
