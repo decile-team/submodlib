a125622196fa28a69fe99684cf890516586bd6fe
Felix Möller <felix.moeller4@gmail.com>
2023-03-18 17:12:19 +0100

    Update setup.py
    Change packages required for install

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ setup(
     #     "tqdm >= 4.24.0",
     #     "nose"
     # ],
-    install_requires=["numpy==1.20.1", "scipy", "sklearn", "numba"],
+    install_requires=["numpy==1.22.0", "scipy", "scikit-learn", "numba"],
     #setup_requires=['pybind11','pytest-runner'],
     tests_require=['pytest'],
     #extras_require={"test": "pytest"},
