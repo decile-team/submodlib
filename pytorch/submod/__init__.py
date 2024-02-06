6cf311be845c93de9a39e47ace811f3e27f5c857
JahanviRajput <142418693+JahanviRajput@users.noreply.github.com>
2024-02-06 18:56:59 +0530

    Update __init__.py

--- pytorch/submod/__init__.py
+++ pytorch/submod/__init__.py
@@ -1,4 +1,8 @@
 # /pytorch/SetFunction/__init__.py
-from .SetCover import SetCover
-from .ProbabilisticSetCover import ProbabilisticSetCover
-from .GraphCut import GraphCut
+from .SetCover import SetCoverFunction
+from .ProbabilisticSetCover import ProbabilisticSetCoverFunction
+from .GraphCut import GraphCutFunction
+from .DisparityMin import DisparityMinFunction
+from .DisparitySum import DisparitySumFunction
+from .FacilityLocation import FacilityLocationFunction
+from .LogDeteminant import LogDeteminantFunction
